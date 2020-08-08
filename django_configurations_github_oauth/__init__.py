d3132c78b58043c9472f57cd1dcd22f3d34cb8bd
andrewp-as-is <russianidiot.github@gmail.com>
2020-08-08 12:15:30 +0300

    2020:08:08 12:15

--- django_configurations_github_oauth/__init__.py
+++ django_configurations_github_oauth/__init__.py
@@ -3,8 +3,8 @@ import os
 from configurations import Configuration, values
 
 APP = 'django_github_oauth'
-AUTH_USER_MODEL = 'django_github_oauth.User'
 AUTHENTICATION_BACKEND = 'django_passwordless_auth.backend.PasswordlessAuthBackend'
+
 
 class GithubOAuthConfiguration(Configuration):
     GITHUB_OAUTH_CLIENT_ID = values.Value(None)
@@ -14,15 +14,9 @@ class GithubOAuthConfiguration(Configuration):
     LOGIN_URL = '/github-oauth/login'
 
     @classmethod
-    def pre_setup(cls):
-        super(GithubOAuthConfiguration, cls).pre_setup()
-        cls.AUTH_USER_MODEL = AUTH_USER_MODEL
-
-    @classmethod
     def setup(cls):
         super(GithubOAuthConfiguration, cls).setup()
         if APP not in cls.INSTALLED_APPS:
             cls.INSTALLED_APPS.append(APP)
         if AUTHENTICATION_BACKEND not in cls.AUTHENTICATION_BACKENDS:
             cls.AUTHENTICATION_BACKENDS.append(AUTHENTICATION_BACKEND)
-
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='django-configurations-github-oauth',
+    version='2020.7.2',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
